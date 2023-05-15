c419511c112f533ab6c4f31225cf686c9577a59a
stefan.nastase1512 <stefan.nastase1512@stud.acs.upb.ro>
2023-05-15 22:32:58 +0300

    Add order execution mechanism when inserting adding orders

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, Response
 import psycopg2
 import json
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -21,22 +22,110 @@ def add_order():
     price = payload['price']
     placed_at = payload['placed_at']
 
+    searched_type = 'S' if type == 'B' else 'B'
+
     connection = None
 
     try:
         connection = psycopg2.connect(host=host, dbname=db_name, user=db_user, password=db_pass)
         cursor = connection.cursor()
 
-        query = " \
-                INSERT INTO placed (\"ClientID\", \"Symbol\", \"Type\", \"Quantity\", \"Price\", \"PlacedAt\") \
-                VALUES (%s, %s, %s, %s, %s, %s) RETURNING \"ID\""
+        if type == 'B':
+            search_query = f"SELECT * FROM placed WHERE \"Symbol\" = '{symbol}' AND \"Type\" = '{searched_type}' ORDER BY \"Price\" ASC"
+        else:
+            search_query = f"SELECT * FROM placed WHERE \"Symbol\" = '{symbol}' AND \"Type\" = '{searched_type}' ORDER BY \"Price\" DESC"
 
-        cursor.execute(query, (client_id, symbol, type, quantity, price, placed_at))
-        id = cursor.fetchone()[0]
-        connection.commit()
+        cursor.execute(search_query)
+
+        records = cursor.fetchall()
+
+        remaining_quantity = quantity
+
+        if len(records) != 0:
+            for row in records:
+                found_id = row[0]
+                found_client_id = row[1]
+                found_symbol = row[2]
+                found_type = row[3]
+                found_quantity = row[4]
+                found_price = float(row[5])
+                price_ok = found_price <= price if type == 'B' else found_price >= price
+
+                if remaining_quantity != 0 and price_ok:
+                    if remaining_quantity < found_quantity:
+                        # ORDER FILLED WITH UPDATE
+                        # INSERT INTO executed ORDER PLACED
+                        query = " \
+                                INSERT INTO executed (\"ClientID\", \"Symbol\", \"Type\", \"Quantity\", \"Price\", \"ExecutedAt\") \
+                                VALUES (%s, %s, %s, %s, %s, %s)"
+                        cursor.execute(query, (client_id, symbol, type, remaining_quantity, found_price, datetime.now().isoformat()))
+                        # INSERT INTO executed ORDER FOUND (partly); 
+                        query = " \
+                                INSERT INTO executed (\"ClientID\", \"Symbol\", \"Type\", \"Quantity\", \"Price\", \"ExecutedAt\") \
+                                VALUES (%s, %s, %s, %s, %s, %s)"
+                        cursor.execute(query, (found_client_id, found_symbol, found_type, remaining_quantity, found_price, datetime.now().isoformat()))
+                        # UPDATE placed WHERE ID = FoundID
+                        query = "UPDATE placed SET \"Quantity\" = %s WHERE \"ID\" = %s"
+                        cursor.execute(query, (found_quantity - remaining_quantity, found_id))
+                        connection.commit()
+
+                        remaining_quantity = 0
+
+                    elif remaining_quantity == found_quantity:
+                        # ORDER FILLED WITH REMOVE
+                        # INSERT INTO executed ORDER PLACED
+                        query = " \
+                                INSERT INTO executed (\"ClientID\", \"Symbol\", \"Type\", \"Quantity\", \"Price\", \"ExecutedAt\") \
+                                VALUES (%s, %s, %s, %s, %s, %s)"
+                        cursor.execute(query, (client_id, symbol, type, remaining_quantity, found_price, datetime.now().isoformat()))
+                        # INSERT INTO executed ORDER FOUND
+                        query = " \
+                                INSERT INTO executed (\"ClientID\", \"Symbol\", \"Type\", \"Quantity\", \"Price\", \"ExecutedAt\") \
+                                VALUES (%s, %s, %s, %s, %s, %s)"
+                        cursor.execute(query, (found_client_id, found_symbol, found_type, found_quantity, found_price, datetime.now().isoformat()))
+                        # REMOVE FROM placed ORDER FOUND
+                        query = f"DELETE FROM placed WHERE \"ID\" = '{found_id}'"
+                        cursor.execute(query)
+                        connection.commit()
+
+                        remaining_quantity = 0
+
+                    else:
+                        # ORDER NOT FILLED
+                        # INSERT INTO executed ORDER PLACED (partly) 
+                        query = " \
+                                INSERT INTO executed (\"ClientID\", \"Symbol\", \"Type\", \"Quantity\", \"Price\", \"ExecutedAt\") \
+                                VALUES (%s, %s, %s, %s, %s, %s)"
+                        cursor.execute(query, (client_id, symbol, type, found_quantity, found_price, datetime.now().isoformat())) 
+                        # INSERT INTO executed ORDER FOUND (partly); 
+                        query = " \
+                                INSERT INTO executed (\"ClientID\", \"Symbol\", \"Type\", \"Quantity\", \"Price\", \"ExecutedAt\") \
+                                VALUES (%s, %s, %s, %s, %s, %s)"
+                        cursor.execute(query, (found_client_id, found_symbol, found_type, found_quantity, found_price, datetime.now().isoformat()))
+                        # REMOVE FROM placed ORDER FOUND
+                        query = f"DELETE FROM placed WHERE \"ID\" = '{found_id}'"
+                        cursor.execute(query)
+                        connection.commit()
+                        
+                        remaining_quantity = remaining_quantity - found_quantity
+                else:
+                    break
+
+        # IF REMAINING QUANTITY NOT NULL
+        # INSERT order INTO placed    
+        if remaining_quantity != 0:             
+            query = " \
+                    INSERT INTO placed (\"ClientID\", \"Symbol\", \"Type\", \"Quantity\", \"Price\", \"PlacedAt\") \
+                    VALUES (%s, %s, %s, %s, %s, %s) RETURNING \"ID\""
+
+            cursor.execute(query, (client_id, symbol, type, remaining_quantity, price, placed_at))
+            id = cursor.fetchone()[0]
+            connection.commit()
         
-        return Response(json.dumps({'id' : id}), status=201, mimetype='application/json')
+            return Response(json.dumps({'id' : id}), status=201, mimetype='application/json')
 
+        return Response(json.dumps({"message": "order placed and filled"}), status=201, mimetype='application/json')
+    
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
         if connection is not None:
